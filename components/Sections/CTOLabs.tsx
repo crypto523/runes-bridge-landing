c499e66d53d461d7673680a5f83fa9c2eb842b0f
crypto523 <jacksoncurry46@gmail.com>
2024-12-07 16:50:47 +0200

    Fix quot error

--- components/Sections/CTOLabs.tsx
+++ components/Sections/CTOLabs.tsx
@@ -17,13 +17,13 @@ const CTOLabs: React.FC = () => {
           <h6 className="text-[#061012] font-[600] font-poppins text-[24px] leading-[36px] mb-[10px]">
             Launch a CTO <br /> “Powered by RunesBridge-V"
           </h6>
-          <p className=" text-[#444444] font-inter, font-[500] text-[16px] leading-[19.36px]">
+          <p className="text-[#444444] font-inter, font-[500] text-[16px] leading-[19.36px]">
             We specialize in helping to launch projects and our niche is the
             “CTO”. <br /><br /> We have animators, raiders, marketers, a growing
             number of KOLs and we also have a growing suite of tech in the
             workshop for anyone to play with. We offer these tools as white
             label products and all the hands-on support needed for your project
-            to launch and maintain successfully.{" "} <br /><br />
+            to launch and maintain successfully. <br /><br />
             Want to know more ? <br /><br />
             Join our discord and open a CTO-Onboarding Ticket to get started.
           </p>
