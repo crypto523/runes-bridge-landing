e7f33d5091f43bb546fb9b762615f7408668ba36
crypto523 <jacksoncurry46@gmail.com>
2024-12-23 14:39:10 +0200

    Update text style

--- components/Sections/RBVToken.tsx
+++ components/Sections/RBVToken.tsx
@@ -14,15 +14,17 @@ const RBVToken: React.FC<RBVTokenProps> = () => {
     return (
         <div id="rbv" className={`w-full md:h-screen flex-1 flex items-center p-5 pt-0 md:py-24 md:px-14 lg:px-10 lg:py-10 lg:portrait:overflow-auto lg:landscape:overflow-hidden`} style={{ opacity: 0 }}>
             <SectionLayout nextSection="RBVTOKEN">
-                <div className="w-2/3 flex-1 flex flex-col items-center justify-center">
-                    <h2 className="font-poppins font-[600] text-[48px] text-primary mb-7">RBV Token</h2>
-                    <div className="w-full flex items-center font-poppins font-[500] text-[24px] text-[#666666] border-[#C4C3CB] border-b-[0.8px]">
-                        <div className="w-1/2 h-full flex items-center justify-end p-2 border-r-[0.8px]"><p className="p-2 w-fit text-right">Total 1,000,000,000 <span className="text-nowrap">(1 Billion Tokens)</span></p></div>
-                        <div className="w-1/2 h-full p-2 border-l-[0.8px]"><p className="p-2 w-fit ">Total Team Tokens 5%</p></div>
-                    </div>
-                    <div className="w-full flex items-center justify-center font-poppins font-[500] text-[24px] text-[#666666] border-[#C4C3CB] border-t-[0.8px]">
-                        <div className="w-1/2 h-full flex items-center justify-end p-2 border-r-[0.8px]"><p className="p-2 w-fit text-right">Total Treasury 600,000,000</p></div>
-                        <div className="w-1/2 h-full p-2 border-l-[0.8px]"><p className="p-2 w-fit ">Total Uniswap LP Seed 300,000,00</p></div>
+                <div className="w-2/3 flex-1 flex flex-col items-center justify-center gap-5">
+                    <h2 className="text-3xl leading-8 md:text-4xl xl:text-5xl">RBV Token</h2>
+                    <div className="w-full">
+                        <div className="w-full flex items-center font-poppins font-[500] text-[24px] text-[#666666] border-[#C4C3CB] border-b-[0.8px]">
+                            <div className="w-1/2 h-full flex items-center justify-end p-2 border-r-[0.8px]"><p className="p-2 w-fit text-right">Total 1,000,000,000 <span className="text-nowrap">(1 Billion Tokens)</span></p></div>
+                            <div className="w-1/2 h-full p-2 border-l-[0.8px]"><p className="p-2 w-fit ">Total Team Tokens 5%</p></div>
+                        </div>
+                        <div className="w-full flex items-center justify-center font-poppins font-[500] text-[24px] text-[#666666] border-[#C4C3CB] border-t-[0.8px]">
+                            <div className="w-1/2 h-full flex items-center justify-end p-2 border-r-[0.8px]"><p className="p-2 w-fit text-right">Total Treasury 600,000,000</p></div>
+                            <div className="w-1/2 h-full p-2 border-l-[0.8px]"><p className="p-2 w-fit ">Total Uniswap LP Seed 300,000,00</p></div>
+                        </div>
                     </div>
                 </div>
             </SectionLayout>
